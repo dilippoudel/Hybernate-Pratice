76498b01c81cef019098e3290ebd008f976d4f8b
Dilip Poudel <dilippoudel578@gmail.com>
2021-02-06 17:45:08 +0200

    querry cache in hibernate

--- DemoHibernate/src/main/java/com/dilip/DemoHibernate/App.java
+++ DemoHibernate/src/main/java/com/dilip/DemoHibernate/App.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.hibernate.service.ServiceRegistry;
 
 /**
@@ -15,7 +16,7 @@ public class App
     public static void main( String[] args )
     {
     	
-    	HardDisk hd = new HardDisk();
+		HardDisk hd = new HardDisk();
     	hd.setHid(301);
     	hd.setType("SDD");
     	
@@ -47,36 +48,44 @@ public class App
     	// create session factory
     	ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
     	
-    	SessionFactory sf = con.buildSessionFactory(registry);    	
+    	SessionFactory sf = con.buildSessionFactory(registry);  
+    	
+    	
     	// saving the data to database
-    	Session session = sf.openSession();
-    	session.beginTransaction();
+//    	Session session = sf.openSession();
+//    	session.beginTransaction();
+//    	   	
+//    	session.save(laptop);
+//    	session.save(courses);
+//    	session.save(std);
+//    	session.save(hd);
+//    	session.getTransaction().commit();
     	
-    	session.save(laptop);
-    	session.save(courses);
-    	session.save(std);
-    	session.save(hd);
-    	session.getTransaction().commit();
     	
     	
-    	Student s1 = session.get(Student.class, 1);
-    	System.out.println(s1);
-    	session.close();
+    	
+    	// fetching data from database using querry
     	
     	Session session1 = sf.openSession();
     	session1.beginTransaction();
-    	Student s2 = session1.get(Student.class, 1);
-    	System.out.println(s2);
+    	Query<Student> q1 = session1.createQuery("from Student where rollId=1");
+    	q1.setCacheable(true);
+    	Student s1 = q1.uniqueResult();
+    	System.out.println(s1);
     	
     	session1.getTransaction().commit();
     	session1.close();
     	
     	
     	
+    	
+    	
     	Session session2 = sf.openSession();
     	session2.beginTransaction();
-    	Student s3 = session2.get(Student.class, 1);
-    	System.out.println(s3);
+    	Query<Student> q2 = session2.createQuery("from Student where rollId=1");
+    	q2.setCacheable(true);
+    	Student s2 = q2.uniqueResult();
+    	System.out.println(s2);
     	
     	session2.getTransaction().commit();
     	session2.close();
